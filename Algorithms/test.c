d9df7c299840e8327c8ad2e0c2d1c2add8ada869
ctuu <ct.liuu@gmail.com>
2016-10-09 22:18:03 +0800

    git commit

--- Algorithms/test.c
+++ Algorithms/test.c
@@ -3,7 +3,7 @@ int main(void)
 {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    if (b < a && c > 0)
+    if ((b < a && c > 0) || (b > a && c < 0))
         printf("NO");
     else if (c == 0)
         if (a == b)
