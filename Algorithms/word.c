449a09ea04cc0c6ee095e7d62a6fb3fb287ba115
ctuu <ct.liuu@gmail.com>
2016-10-10 20:17:18 +0800

    git commit

--- Algorithms/word.c
+++ Algorithms/word.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 int main(void)
 {
-    int word[15][15];
-    int num, count;
-    int a, b;
-    scanf("%d", &num);
-    while (getchar != '\n')
-    {
-        for (a = 0; a < num; a++)
-            for (b = 0; getchar() != ' '; b++)
-                word[a][b] = getchar();
-    }
-    for (a = 0; a < num; a++)
-        for (b = 0; b < 15; b++)
-            putchar(word[a][b]);
-
     return 0;
 }
