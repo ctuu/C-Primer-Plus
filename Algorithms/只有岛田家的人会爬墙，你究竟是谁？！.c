617e99ce586f1d8fa6baadb3fd9b5da106b7dde2
ctuu <ct.liuu@gmail.com>
2016-10-11 22:27:44 +0800

    git

--- Algorithms/只有岛田家的人会爬墙，你究竟是谁？！.c
+++ Algorithms/只有岛田家的人会爬墙，你究竟是谁？！.c
@@ -5,18 +5,20 @@ int main(void)
     int a, b, t, day;
     while (scanf("%d", &day) != EOF)
     {
+ 
         for (a = 0, b = 1, t = 0; day > 0; day--)
         {
             t = b;
             b += a;
             a = t;
-        }
-        if (b >= HEIGHT)
-            {printf("YES\n");
-                        printf("%d %d %d\n", HEIGHT, b, HEIGHT - b);
+            if ( b >= HEIGHT)
+            {  
+                printf("YES\n");
+                break;
             }
-        else
-            printf("%d %d %d\n", HEIGHT, b, HEIGHT - b);
+        }
+            if ( b < HEIGHT )
+            printf("%d\n", HEIGHT - b);
     }
     return 0;
 }
