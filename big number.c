c5285c5ecee5320c591b72ec36ba7c3b034e75c5
ctuu <ct.liuu@gmail.com>
2016-10-23 22:57:25 +0800

    linux

--- big number.c
+++ big number.c
@@ -9,7 +9,8 @@ int main(void)
     for (a = 0; a < NUM; a++)
         for (b = 1; (num[a][b] = getchar()) != '\n' && b < MAX; b++)
             n[a] = b;
-    for (a = 0; a < NUM; a++)
+
+   /* for (a = 0; a < NUM; a++)
         for (c = 0, b = 1; b < MAX; b++)
             if (num[a][b] == 10 || c == 1)
             {
@@ -18,13 +19,26 @@ int main(void)
             }
             else
                 printf("%d ", num[a][b] - 48);
-    
-        for (a = 0; a < NUM; a++)
-    {
-        for (c = 1000, b = n[a]; b <= 1000; b++, c--)
+                ;*/
+
+    for (a = 0; a < NUM; a++)
+        for (c = 1000, b = n[a] + 1; b > 0; b--, c--)
         {
+            if (num[a][b] == 10)
+                {
+                    num[a][b] = 0;
+                    c = 1001;
+                }
+            else
             num[a][c] = num[a][b] - 48;
             num[a][b] = 0;
         }
-    }
+
+    for (a = 0; a < NUM; a++)
+        {
+        for (b = 1; b < MAX; b++)
+            printf("%d ", num[a][b]);
+        printf("\n\n");
+        }
+    return 0;
 }
--- temp.c
+++ temp.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#define NUM 2
 int main(void)
 {
-    char a[3] = {1, 8, 3};
-    char b[3];
-    b[1] = a[0] + a[1];
-    printf("%d", b[1]);
-    return 0;
+    int a, b, c;
+    int n[NUM]= {2, 4};
+    for (a = 0; a < NUM;a++)
+        for (c = 1000, b = n[a];b > 0; b--, c--)
+        {
+            printf("%d",b);
+        }
 }
