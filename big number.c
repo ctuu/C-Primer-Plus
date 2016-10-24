76b47b4d853bf71b44f62b11c0613038c085163c
ctuu <ct.liuu@gmail.com>
2016-10-24 16:17:33 +0800

    git

--- big number.c
+++ big number.c
@@ -1,44 +1,76 @@
 #include <stdio.h>
 #define NUM 2
-#define MAX 1001
+#define MAX 1002
 int main(void)
 {
-    int a, b, c;
+    int a, b, c, count, t;
     int n[NUM];
-    int num[NUM + 1][MAX] = {0};
-    for (a = 0; a < NUM; a++)
-        for (b = 1; (num[a][b] = getchar()) != '\n' && b < MAX; b++)
-            n[a] = b;
+    scanf("%d", &count);
+    while (getchar() != '\n')
+        continue;
+    for (t = 1; t <= count; t++)
+    {
+        printf("Case %d:\n", t);
 
-   /* for (a = 0; a < NUM; a++)
-        for (c = 0, b = 1; b < MAX; b++)
-            if (num[a][b] == 10 || c == 1)
+        int num[NUM + 1][MAX] = {0};
+
+        for (a = 0; a < NUM; a++)
+            for (b = 1; (num[a][b] = getchar()) != '\n' && b < MAX; b++)
+                n[a] = b;
+
+        for (a = 0; a < NUM; a++)
+            for (c = MAX - 1, b = n[a] + 1; b > 0; b--, c--)
+            {
+                if (num[a][b] == 10)
+                {
+                    num[a][b] = 48;
+                    c = MAX;
+                }
+                num[a][c] = num[a][b] - 48;
+                num[a][b] = 0;
+            }
+
+        for (a = 0; a < NUM; a++)
+            for (b = 1; b < MAX; b++)
+                num[NUM][b] += num[a][b];
+
+        for (b = MAX - 1; b > 0; b--)
+            while (num[NUM][b] >= 10)
+            {
+                num[NUM][b] -= 10;
+                num[NUM][b - 1] += 1;
+            }
+        
+        for (a = 0; a < NUM; a++)
+        {
+            for (c = 0, b = 0; b < MAX; b++)
+            {
+                if (num[a][b] == 0 && c == 0)
+                    continue;
+                else
+                {
+                    c = 1;
+                    printf("%d", num[a][b]);
+                }
+            }
+            if (a < NUM - 1)
+                printf(" + ");
+        }
+
+        printf(" = ");
+        for (c = 0, b = 0; b < MAX; b++)
+        {
+            if (num[NUM][b] == 0 && c == 0)
+                continue;
+            else
             {
                 c = 1;
-                continue;
+                printf("%d", num[NUM][b]);
             }
-            else
-                printf("%d ", num[a][b] - 48);
-                ;*/
-
-    for (a = 0; a < NUM; a++)
-        for (c = 1000, b = n[a] + 1; b > 0; b--, c--)
-        {
-            if (num[a][b] == 10)
-                {
-                    num[a][b] = 0;
-                    c = 1001;
-                }
-            else
-            num[a][c] = num[a][b] - 48;
-            num[a][b] = 0;
         }
 
-    for (a = 0; a < NUM; a++)
-        {
-        for (b = 1; b < MAX; b++)
-            printf("%d ", num[a][b]);
-        printf("\n\n");
-        }
+        if (t != count)
+            printf("\n\n");
+    }
     return 0;
 }
