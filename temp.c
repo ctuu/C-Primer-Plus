a492ec7810f82c30123c74e0cc4a103575f6dc1b
ctuu <ct.liuu@gmail.com>
2016-12-13 22:07:23 +0800

    changed

--- temp.c
+++ temp.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    int a = 2;
-    int c = 3;
-    int * p[2] = {&a, &c};
-    int * t;
-    int b = 0;
-    for (t = p[0];t && b < 2; b++)
-    {
-        printf("%d\n", *t);
-        t = p[1];
-    }
+    int d = 14;
+    printf("%o %x", d, d);
     return 0;
 }
